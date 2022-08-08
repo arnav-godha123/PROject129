17c01554f7b32e30fcf1c41c592a894dd0b71f84
arnav-godha123 <85485288+arnav-godha123@users.noreply.github.com>
2022-08-08 16:07:01 +0530

    Add files via upload

---
+++ archive_sorted.py
@@ -0,0 +1,27 @@
+import csv 
+data=[]
+with open('archive_dataset.csv',"r") as f:
+    csv_reader=csv.reader(f)
+    for row in csv_reader:
+        data.append(row)
+
+headers=data[0]
+planet_data=data[1:]
+#print (planet_data)
+
+for data_point in planet_data:
+     data_point[2] = data_point[2].lower()
+    #Sorting planet names in alphabetical order 
+planet_data.sort(key=lambda planet_data: planet_data[2])
+with open("archive_dataset_sorted2.csv","a+") as f:
+    csv_writer=csv.writer(f)
+    csv_writer.writerow(headers)
+    csv_writer.writerows(planet_data)
+
+    
+with open('archive_dataset_sorted2.csv') as input, open('archive_dataset_sorted3.csv', 'w', newline='')as output:
+   writer = csv.writer(output)
+   for row in csv.reader(input):
+     if any(field.strip() for field in row):
+         writer.writerow(row)
+ 
